bc4707efa1883212e6a6cbec5bcaa17c72c0a512
JoshClark7 <joshtclark7@gmail.com>
2023-05-02 14:18:36 -0400

    Added unity project

---
+++ Neon Chamber Heaven/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Control Options")]
+    public Vector2 sensitvity;
+}
